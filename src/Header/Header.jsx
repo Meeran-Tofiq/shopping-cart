ce032300f59e63c664d282a0e6b0369a9cb8ba4e
Meeran Tofiq <101665499+Meeran-Tofiq@users.noreply.github.com>
2023-12-05 17:28:05 +0300

    Fix links list not working

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 let categories = [
 	"Men's Clothing",
 	"Women's Clothing",
-	"Jewelry",
+	"Jewelery",
 	"Electronics",
 ];
 
@@ -14,9 +14,9 @@ export default function Header({ category }) {
 				<Link to="/">FAKKE STORE</Link>
 			</h1>
 			<ul>
-				{categories.map((cat) => (
+				{categories.map((cat, i) => (
 					<li key={cat} className="">
-						{cat}
+						<Link to={"/store/" + cat.toLowerCase()}>{cat}</Link>
 					</li>
 				))}
 			</ul>

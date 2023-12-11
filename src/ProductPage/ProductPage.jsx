398ab90cb84be7bd7ca70f858679c15ecda30f0f
Meeran Tofiq <101665499+Meeran-Tofiq@users.noreply.github.com>
2023-12-11 20:58:19 +0300

    Feat page to buy qnty of any product you click

---
+++ src/ProductPage/ProductPage.jsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+import { useLocation } from "react-router-dom";
+import Header from "../Header/Header";
+import { Link } from "react-router-dom";
+
+export default function ProductPage() {
+	let { state } = useLocation();
+	let product = state.product;
+	let [quantity, setQuantity] = useState(0);
+
+	function increasequantity() {
+		setQuantity(quantity + 1);
+	}
+
+	function decreasequantity() {
+		if (quantity === 0) return;
+		setQuantity(quantity - 1);
+	}
+
+	return (
+		<>
+			<Header />
+			<main>
+				<img src={product.image} />
+				<h2>{product.title}</h2>
+				<span>{product.price}</span>
+				<p>{product.description}</p>
+
+				<div>
+					<span>{quantity}</span>
+					<button onClick={() => increasequantity()}>+</button>
+					<button onClick={() => decreasequantity()}>-</button>
+					<Link to=".." relative="path">
+						<button>Add to Cart</button>
+					</Link>
+				</div>
+			</main>
+		</>
+	);
+}

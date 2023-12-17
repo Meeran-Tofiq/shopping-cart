e2ac5fa3844830d4a4ebc0450af8666dc68b32d4
Meeran Tofiq <101665499+Meeran-Tofiq@users.noreply.github.com>
2023-12-17 13:46:37 +0300

    Style header

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -1,4 +1,6 @@
 import { Link } from "react-router-dom";
+import styles from "./Header.module.css";
+import { useState } from "react";
 
 let categories = [
 	"Men's Clothing",
@@ -7,20 +9,30 @@ let categories = [
 	"Electronics",
 ];
 
-export default function Header({ category }) {
+export default function Header() {
+	let [active, setActive] = useState("");
+
+	function handleChangingActiveCategory(cat) {
+		setActive(cat);
+	}
+
 	return (
-		<header>
+		<header className={styles.header}>
 			<h1>
 				<Link to="/">FAKKE STORE</Link>
 			</h1>
 			<ul>
 				{categories.map((cat, i) => (
-					<li key={cat} className="">
+					<li
+						key={cat}
+						className={active === cat ? styles.active : ""}
+						onClick={() => handleChangingActiveCategory(cat)}
+					>
 						<Link to={"/store/" + cat.toLowerCase()}>{cat}</Link>
 					</li>
 				))}
 			</ul>
-			<Link to="cart">
+			<Link to="cart" className={styles.link}>
 				<span>CART</span>
 			</Link>
 		</header>

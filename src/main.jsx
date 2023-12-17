d630731ac9b5d0b81bfcf6cf6dcde4d77b0928d2
Meeran Tofiq <101665499+Meeran-Tofiq@users.noreply.github.com>
2023-12-17 13:36:11 +0300

    Style body to have no margin

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import "./styles.css";
 
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Homepage from "./Homepage/Homepage";

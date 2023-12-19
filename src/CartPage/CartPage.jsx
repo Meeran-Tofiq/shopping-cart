150497a4957972072b0c34e24368e9344dc8a8dd
Meeran Tofiq <101665499+Meeran-Tofiq@users.noreply.github.com>
2023-12-19 18:00:14 +0300

    Style empty cart page

--- src/CartPage/CartPage.jsx
+++ src/CartPage/CartPage.jsx
@@ -1,18 +1,17 @@
 import { useOutletContext } from "react-router-dom";
+import styles from "./CartPage.module.css";
 
 export default function CartPage() {
 	const [cartItems, setCartItems] = useOutletContext();
 
 	if (cartItems.length < 1) {
 		return (
-			<>
-				<div>
-					<h1>Uh oh...</h1>
-					<h3>
-						Your cart appears to be empty... Head to the shop to fill it up!
-					</h3>
-				</div>
-			</>
+			<main className={styles.empty}>
+				<h1>Uh oh...</h1>
+				<h3>
+					Your cart appears to be empty... Head to the shop to fill it up!
+				</h3>
+			</main>
 		);
 	}
 
@@ -47,37 +46,33 @@ export default function CartPage() {
 
 	return (
 		<main>
-			<>
-				<ul>
-					{cartItems.map((product) => (
-						<li key={product.id}>
-							<div>
-								<img src={product.image} />
-								<h2>{product.title}</h2>
-								<span>{product.price}</span>
-							</div>
-							<div>
-								<span>{product.quantity}</span>
-								<span>{product.price * product.quantity}</span>
-								<button onClick={() => increaseQuantityOfItem(product)}>
-									+
-								</button>
-								<button
-									onClick={() => {
-										if (product.quantity > 1) decreaseQuantityOfItem(product);
-										else removeItemFromCart(product);
-									}}
-								>
-									-
-								</button>
-							</div>
-							<button onClick={() => removeItemFromCart(product)}>TRASH</button>
-						</li>
-					))}
-				</ul>
-				<span>TOTAL</span>
-				<span>${total}</span>
-			</>
+			<ul>
+				{cartItems.map((product) => (
+					<li key={product.id}>
+						<div>
+							<img src={product.image} />
+							<h2>{product.title}</h2>
+							<span>{product.price}</span>
+						</div>
+						<div>
+							<span>{product.quantity}</span>
+							<span>{product.price * product.quantity}</span>
+							<button onClick={() => increaseQuantityOfItem(product)}>+</button>
+							<button
+								onClick={() => {
+									if (product.quantity > 1) decreaseQuantityOfItem(product);
+									else removeItemFromCart(product);
+								}}
+							>
+								-
+							</button>
+						</div>
+						<button onClick={() => removeItemFromCart(product)}>TRASH</button>
+					</li>
+				))}
+			</ul>
+			<span>TOTAL</span>
+			<span>${total}</span>
 		</main>
 	);
 }

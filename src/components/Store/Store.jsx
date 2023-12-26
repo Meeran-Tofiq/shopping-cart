605add750cde107000e4f97cb87bcfef3be66859
Meeran Tofiq <101665499+Meeran-Tofiq@users.noreply.github.com>
2023-12-26 17:26:34 +0300

    Style store component

--- src/components/Store/Store.jsx
+++ src/components/Store/Store.jsx
@@ -1,13 +1,14 @@
 import Products from "../Products/Products";
 import { useParams } from "react-router-dom";
+import styles from "./Store.module.css";
 
 export default function Store() {
 	let { category } = useParams();
 
 	return (
 		<>
-			<main>
-				<h1>{category.toUpperCase()}</h1>
+			<main className={styles.main}>
+				<h1 className={styles.heading}>{category.toUpperCase()}</h1>
 				<Products category={category}></Products>
 			</main>
 		</>
